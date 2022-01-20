c195ddc4407144cb03730d13eb96e9bd0a8cff63
d4devil <damian.moreno@wizeline.com>
2022-01-20 11:54:29 -0600

    Fixed: trainers controller

--- app/controllers/trainers_controller.rb
+++ app/controllers/trainers_controller.rb
@@ -3,25 +3,38 @@ class TrainersController < ApplicationController
     @trainers = Trainer.all
   end
 
-  def create
-  end
-  
-  def new
-    
-  end
-  
-  def edit
-    
-  end
-  
   def show
+    @trainer = Trainer.find_by_id params[:id]
+  end
+
+  def new
+    @trainer = Trainer.first
+  end
+
+  def create
+    @trainer = Trainer.new(trainer_params)
+
+    if @trainer.save
+      redirect_to trainers_path
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @trainer = Trainer.find_by_id params[:id]
   end
 
   def update
-
+    #bulk update to trainer params using 'Strong Paramas'
   end
-  
-  def destoy
 
+  def destoy
+    redirect_to trainers_path
+  end
+
+  private
+  def trainer_params
+    params.require(:trainer).permit(:name)
   end
 end
