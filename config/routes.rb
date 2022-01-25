f22ad164f1f844000d0b622a924022b4753e07a2
d4devil <damian.moreno@wizeline.com>
2022-01-25 13:15:40 -0600

    WIP devise error

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'pokemons#index'
+
+  devise_for :users
   resources :egg_groups
   resources :pokemons
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_24_201402) do
+ActiveRecord::Schema.define(version: 2022_01_25_174738) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,20 @@ ActiveRecord::Schema.define(version: 2022_01_24_201402) do
     t.index ["egg_group_id"], name: "index_pokemons_on_egg_group_id"
     t.index ["evolves_to_pokemon_id"], name: "index_pokemons_on_evolves_to_pokemon_id"
     t.index ["variety_of_pokemon_id"], name: "index_pokemons_on_variety_of_pokemon_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
+    t.string "username"
+    t.string "hash_password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "pokemons", "egg_groups"
