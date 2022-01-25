576f440a843c9f37ea03d5b1002962c74ff2d7d1
d4devil <damian.moreno@wizeline.com>
2022-01-25 11:28:06 -0600

    WIP: data validation

--- app/models/egg_group.rb
+++ app/models/egg_group.rb
@@ -1,3 +1,7 @@
 class EggGroup < ApplicationRecord
+  # One-to-many relationship between EggGroup and Pokemon
   has_many :pokemons
+
+  # Data validations
+  
 end
--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,9 +1,14 @@
 class Pokemon < ApplicationRecord
+  # One-to-many relationship (self-join) between Pokemon and Pokemon(Variant)
   has_many :variants, class_name: 'Pokemon', foreign_key: :variety_of_pokemon_id
   belongs_to :origin, class_name: 'Pokemon', optional: true, foreign_key: :variety_of_pokemon_id
-
+  
+  # One-to-one relationship (self-join) between Pokemon and Pokemon(Evolution)
   has_one :evolution, class_name: 'Pokemon', foreign_key: :evolves_to_pokemon_id
   belongs_to :org_evolution, class_name: 'Pokemon', optional: true, foreign_key: :evolves_to_pokemon_id
 
+  # One-to-many relationship between EggGroup and Pokemon
   belongs_to :egg_group
+
+  # Data validations
 end
